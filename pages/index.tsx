78b843bd6dc80854b1e3b57b0f59a1c4faa0f432
Prabhjot Singh <prabhjotchugh0805@gmail.com>
2023-08-21 20:21:11 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NextPageContext } from 'next';
 import { getSession } from 'next-auth/react';
+import Head from 'next/head';
 
 import Navbar from '@/components/Navbar';
 import Billboard from '@/components/Billboard';
@@ -34,6 +35,10 @@ const Home = () => {
 
   return (
     <>
+      <head>
+        <title>Netflix</title>
+        <meta name="google-site-verification" content="SGBiyPh3FHueDts8YTHlX7dV_P0cw4c-a83rTufAOvU" />
+      </head>
       <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
